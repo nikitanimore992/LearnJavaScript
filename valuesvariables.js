6c6b8b90a68d05984b0011b0f0034954947a16a8
nikita nimore <992nikitanimore@gmail.com>
2024-11-08 21:41:40 +0530

    create a new page values and varibale in javaScript

---
+++ valuesvariables.js
@@ -0,0 +1,11 @@
+
+
+
+
+var myName = 'nikita';
+console.log(myName);
+
+// consol pr kuch bhi print karane  ke liye ham consol.log(variable_name) likhte hai 
+/*
+first letter must be letter or underscore(_)
+*/

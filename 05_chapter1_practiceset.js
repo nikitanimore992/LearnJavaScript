39bb417032fe662ad124ed5539c13c188480c824
nikita nimore <992nikitanimore@gmail.com>
2024-11-11 16:51:06 +0530

    complete 05 file that chapter 1 all questions

--- 05_chapter1_practiceset.js
+++ 05_chapter1_practiceset.js
@@ -35,3 +35,20 @@ console.log(item)
  
 //  direct object ko koi value ya data assien nahi kar sakte but usi object ke rafrence
 //  se ap key and value chage kar sakte hai or add bhi kar skte  hai new data 
+
+// chapter 1= Q5 write a javaScript program to create a word meaning dicnory of  words;
+
+const disc = {
+    appreciate:"o enjoy something or to understand the value of somebody/something",
+    Selfie:"just won World of the Year from the Oxford English Dictionary.",
+    Love:"We love you Love is blind",
+    Novel:"The Grapes of Wrath is John Steinbeck's best novel",
+    Season:"The seasons are spring, summer, autumn, and winter"
+
+
+}
+// console.log(disc.appreciate)
+console.log(disc['appreciate'])
+
+
+// to way to acces the object item and value.

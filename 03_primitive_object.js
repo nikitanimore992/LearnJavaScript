0d4085273078bf0c70b3444b1dbafb804165642f
nikita nimore <992nikitanimore@gmail.com>
2024-11-11 16:00:38 +0530

    create a js file that is 03_primitive_object

---
+++ 03_primitive_object.js
@@ -0,0 +1,45 @@
+
+
+
+// nn bb ss u
+// null , number , bite , BigInt , String , Symbol , Undefined
+
+// exa. 
+
+let a = null;
+let b = 123;
+let c = true;
+let d = BigInt('157')+BigInt("5")
+let e = "Nikita"
+let f = Symbol("hello i am learning javaScript")
+let g 
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+
+console.log(a,b,c,d,e,f,g)
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+
+
+// object = none primitive data type 
+
+// exa . 
+const item ={
+    "Nikita": true,
+    "yogita": false,
+    "Neha": 67,
+    "Aman":undefined
+
+}
+console.log(item["Neha"]);

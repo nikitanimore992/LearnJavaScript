63c4909c91b3dcae81e3b2d6df5677d964aa60c7
nikita nimore <992nikitanimore@gmail.com>
2024-11-13 10:47:46 +0530

    complete my 06_operators.js page

--- 06_operators.js
+++ 06_operators.js
@@ -1,1 +1,61 @@
 
+
+
+// 77;
+// "hello Nikita"
+// false
+// all Valid
+
+console.log("operator js page");
+
+// Arithmatic Operators
+
+let a = 10;
+let b = 4;
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a / b = ",a/b);
+console.log("a % b = ",a%b);
+console.log("++a = ",++a);
+console.log("a++ = ",a++);
+console.log("a = ",a);
+console.log("a-- = ",a--);
+console.log("--a = ",--a);
+console.log("a = ",a);
+
+// assinment Operators
+let c = 10;
+c+= 5; // same as a c+5 
+c-=10; // same as a c-5 
+c%=5; // same as a c%5 
+c/=5; // same as a c/5 
+
+console.log("c + 10 = " , c+10);
+
+// coparrison Operators
+
+let comp1 =10;
+let comp2 = 11;
+
+console.log("comp1 ==comp2 is ",comp1==comp2)
+console.log("comp1 !=comp2 is ",comp1!=comp2)
+console.log("comp1 ===comp2 is ",comp1===comp2)
+console.log("comp1 !==comp2 is ",comp1!==comp2)
+
+// let take another example 
+let i =11;
+let j = "11";
+console.log("i == j is ",i==j);
+console.log("i != j is ",i!=j);
+console.log("i === j is ",i===j);
+console.log("i !== j is ",i!==j);
+
+// logical operators
+
+let x =10;
+let y = 12;
+
+console.log("x<y && x==10",x<y && x==10)
+console.log("x>y && x==10",x>y && x==10)
+console.log("x>y || x==10",x<y || x==10)
+

a4a04d61990c65ea56433290ee9cf819d8c5a268
nikita nimore <992nikitanimore@gmail.com>
2024-11-14 14:09:55 +0530

    compelete my 07_conditinal.js page

--- 07_condiation.js
+++ 07_condiation.js
@@ -1,3 +1,15 @@
+let a = prompt("age"); // prompt alway take string type input 
+console.log(a) 
+a = parseInt(a); // parsiInt used for converting stringbto integer 
 
+if(a>0){
+    console.log("your are valid");
+    alert("valid")
+}else{
+    console.log("your are not valid");
+    alert("invalid")
 
+}
 
+// let a =17;
+console.log("age is : ", a>18? "valid":"invalid")

95b44bcc8a9a42d6757969591505bbb56e9490f1
nikita nimore <992nikitanimore@gmail.com>
2024-11-18 18:14:11 +0530

    complete a day 13 day intro string

---
+++ 13_IntroString.js
@@ -0,0 +1,31 @@
+
+
+
+
+let str = "hy i am nikita"
+let st = 'i am fine'
+let s = `how are you`
+// console.log(str,st,s)
+console.log(str)
+console.log(st)
+console.log(s)
+
+
+// Scape seqence character 
+/* if you try to print the following string , javaScript will missunderstand it 
+ 
+ let name = Ma'am
+ 
+ we can use single quote ecape seqence to solve the proble like this 
+ exa - let name = 'ma\'am'
+ 
+ similarly we can use \" inside a string with double quots
+ 
+ other escape sequence character are as follow
+  \n ---> for new line
+  \t ---> for tap
+  \r ---> for carring Return 
+  */
+  let name = 'Ma\'am'
+  let nm = "Ma\'am"
+  let us = "Hii Nikita \n i am fine\tdude"

ae3a5f1a063573895b7d40691bb5c902cc9ed14a
nikita nimore <992nikitanimore@gmail.com>
2024-11-18 14:41:05 +0530

    complete the learn  while loop concept

---
+++ 10_whileloop.js
@@ -0,0 +1,22 @@
+
+// -----------------while loop --------------
+
+let n = prompt("enter any number : ")
+n = Number.parseInt(n);
+let i =0;
+while(i<n){
+    console.log(i)
+    i++;
+    
+}
+
+
+//-------------  do while loop --------------
+
+let num = prompt("enter second num :")
+num = Number.parseInt(num)
+let j = 0;
+do{
+     console.log(j)
+    j++;
+}while(j<num)

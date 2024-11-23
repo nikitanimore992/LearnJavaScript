fa9926ba1a777cb4ad131a737c0bc33ef8222ee9
nikita nimore <992nikitanimore@gmail.com>
2024-11-23 15:15:35 +0530

    compete day 15 practice set

--- 15_chapter4_practiceset.js
+++ 15_chapter4_practiceset.js
@@ -36,3 +36,23 @@ function ke start me koi word hai ya nahi ye find
  /* endstwith function ka use ham kisi particular 
  function ke end me koi word hai ya nahi ye find
   karne ke liye karte hai  */
+
+
+//   3.    Write a program to convert a given string to lowercase.
+
+console.log(stns.toLowerCase());
+
+
+// 4.    Extract the amount out of this string “Please give Rs 1000”
+
+const str = "Please give Rs 1000"
+const str2 = str.slice(15);
+console.log(str2)
+
+const str3 = "Please give Rs 1000 and Remember it"
+const str4 = str3.slice(15,19);
+console.log(str4)
+
+// 5.    Try to change the 4th character of a given string. Were you able to do it?
+const str5 = "hello i am Rama"
+console.log(str5.replace("h","c"));

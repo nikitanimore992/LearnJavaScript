87d1fb8aff9fbc8ab41fc348be48d612fe60ee68
nikita nimore <992nikitanimore@gmail.com>
2024-11-26 18:03:30 +0530

    complete array introduction day 16

--- 16_Array.js
+++ 16_Array.js
@@ -1,1 +1,17 @@
 
+// simple way to create array 
+
+const a = [1,2,3,4,5];
+
+// javaScript Array can store multiple type of value in one array
+
+const marks_of_student = [83,98,91,65,false,"not giving"];
+
+console.log(marks_of_student);
+// if you want to get a particulary student marks you can easily  get by using ndex number for exa 
+console.log(marks_of_student[0])
+// if you want to get length of that array you can simply return for exa 
+console.log(marks_of_student.length)
+marks_of_student[6] = 95; // adding a new value from an array;
+marks_of_student[0] = 99; // changing the value of array index 0
+console.log(marks_of_student)

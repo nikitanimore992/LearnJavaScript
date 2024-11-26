e4a19459f742d3cd77d5ad7d2433fa4e50a30395
nikita nimore <992nikitanimore@gmail.com>
2024-11-26 18:43:44 +0530

    compelete day 17 some array methods

--- 17_Array_Methods.js
+++ 17_Array_Methods.js
@@ -1,1 +1,22 @@
 
+
+// array methodes
+
+let num = [12,13,14,15,16]; // taken a simple array 
+console.log(num);
+let str = num.toString(); // for convert an array to string
+console.log(str);
+let join = num.join("_") // a join function in array it is join every String to using sparater which is given by programmer
+console.log(join);
+let join1 = num.join(",");
+console.log(join1);
+num.pop(); // simply use for delete element in last;
+console.log(num);
+num.push(56) // if you want to push a new element is current array 
+console.log(num);
+num.shift() // it is use for shift first element of the array 
+console.log(num)
+num.unshift(10); // it is use for unshift first element of the array 
+console.log(num)
+// delete num[1];
+// console.log(num)

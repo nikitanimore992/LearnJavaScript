8aa4f8314cb1a6b6fa0d34f5a84b4f3678c7a6a7
nikita nimore <992nikitanimore@gmail.com>
2024-11-26 18:14:33 +0530

    print sucssefully array element by using for loop

--- QuickQize.js
+++ QuickQize.js
@@ -7,3 +7,11 @@ for(let i=0; i<=name.length; i++){
 }
 
 console.log(name.length);
+
+// print all array elements by using for loop
+
+let Math_marks = [56,65,89,85,86,85,33]
+// const length = Math_marks.length;
+for(let i = 0; i<Math_marks.length; i++){
+    console.log(Math_marks[i])
+}

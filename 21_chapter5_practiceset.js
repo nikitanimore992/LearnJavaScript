35e9ae7022d5986f9d399b446eb7e01dd8a58c08
nikita nimore <992nikitanimore@gmail.com>
2025-02-10 23:24:50 +0530

    solve q second in my prectice set

--- 21_chapter5_practiceset.js
+++ 21_chapter5_practiceset.js
@@ -3,10 +3,22 @@
 
 
 
-// problem number 1 
+// problem number 1 : Create an array of numbers and take input from the user to add numbers to this array.
 
 let arr = [10,20,30,1,5,6]
 let a = prompt("Enter any number that you want to add in array = ")
 a=Number.parseInt(a);
 arr.push(a)
 console.log(arr)
+
+// problem number 2 : Keep adding numbers to the array in question-1 until 0 is added the array.
+
+let arr1 = [1,2,3,4,5]
+let b;
+do{
+    b=prompt("Enter Number : ")
+    b = parseInt(b)
+    arr.push(a);
+    
+}while(b!=0)
+console.log(arr1);

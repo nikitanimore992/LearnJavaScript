d5a0c471bb60961c7d475ad05fc2d892225c979d
nikita nimore <992nikitanimore@gmail.com>
2025-02-10 23:09:29 +0530

    solve 1 que of my practice set

---
+++ 21_chapter5_practiceset.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// problem number 1 
+
+let arr = [10,20,30,1,5,6]
+let a = prompt("Enter any number that you want to add in array = ")
+a=Number.parseInt(a);
+arr.push(a)
+console.log(arr)

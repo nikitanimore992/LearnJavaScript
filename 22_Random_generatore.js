497b162628b5de805f8a079053b0d8a3aab8a1c4
nikita nimore <992nikitanimore@gmail.com>
2025-02-10 23:51:25 +0530

    create a new java file which is random generator

---
+++ 22_Random_generatore.js
@@ -0,0 +1,12 @@
+
+
+/*  1.Generate a random number using the Math.random() function and store it in a variable.
+    2.Take input from the user using the prompt() function and store it in a variable.
+    3.Compare the user's guess with the generated number using if-else statements and inform
+     the user accordingly.
+    4.Repeat steps 2 and 3 until the user guesses the correct number.
+    5.Keep track of the number of turns it took the user to guess the correct number and 
+    calculate the score accordingly.
+    6.Terminate the program and show the final score.*/
+
+    

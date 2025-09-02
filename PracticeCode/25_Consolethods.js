366e4685e29d547884154489e92551434af45090
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-03 00:06:48 +0530

    methods

--- PracticeCode/25_Consolethods.js
+++ PracticeCode/25_Consolethods.js
@@ -6,3 +6,26 @@ console.warn("warn")
 console.error("error")
 console.assert("eer" != false)
 console.assert("err" == false)
+// console.table(console);
+console.time("a");
+console.timeEnd("a");
+console.time("Starting this process")
+console.time("p1")
+console.timeEnd("p1")
+
+// Example
+console.time("forLoop")
+for(let i =0; i<5; i++){
+    console.log(223)
+}
+
+console.timeEnd("forLoop")
+
+console.time("WhileLoop")
+ let i=0;
+while(i<5){
+    console.log(233)
+    i++;
+} 
+
+console.timeEnd("WhileLoop")

f3dc7a15c8ae8f8009aacb7a87386128eec41bc4
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-03 19:45:35 +0530

    use alert, propmt, document

---
+++ PracticeCode/27_Alertpromptconfirm.js
@@ -0,0 +1,7 @@
+
+alert("Enter the value of a");
+// iska use website pr alert show karane ke liye kiya jata hai
+let a = prompt("Enter value here ");
+// prompt ka use ham user se input lene ke liye karte he 
+document.write(a); 
+// document.write ka use ham apne wab page pr output show karane ke liye karte he 

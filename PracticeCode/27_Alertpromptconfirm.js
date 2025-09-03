cc3726ebcc692fab34f4411b6f84515f768ed7dd
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-03 19:52:48 +0530

    parseInt, type of

--- PracticeCode/27_Alertpromptconfirm.js
+++ PracticeCode/27_Alertpromptconfirm.js
@@ -5,3 +5,6 @@ let a = prompt("Enter value here ");
 // prompt ka use ham user se input lene ke liye karte he 
 document.write(a); 
 // document.write ka use ham apne wab page pr output show karane ke liye karte he 
+a = Number.parseInt(a); // iska use kisi bhi string number ko integer me convert karta he 
+
+alert("Yor entered a of type " + (typeof a)); // type of function ka use ham enter ki value ka type janne ke liye karnte hai

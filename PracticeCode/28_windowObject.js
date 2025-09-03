1fc493dfd0dd3189398d2eec3730af896466bf09
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-03 22:36:21 +0530

    window object - browser object models and document object model

---
+++ PracticeCode/28_windowObject.js
@@ -0,0 +1,14 @@
+console.log(window)
+// we can write window object like this ---
+window.console.log(window)
+
+console.log(document.body)
+// document.body.style.background="red"
+document.body.style.background="yellow"
+
+
+
+
+
+
+

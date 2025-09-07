9745c8fd8900b0cc1f8c4c63dc7f9d1e1cf5d3d2
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-07 23:09:19 +0530

    practice Q1

--- PracticeCode/29_chapter6_practiceset.js
+++ PracticeCode/29_chapter6_practiceset.js
@@ -1,2 +1,19 @@
+// practice que 1
 
+let age = prompt("Enter a Number ","24")
+age = Number.parseInt(age)
+
+//create a function
+const canDrive = (age)=>{
+    return age>=18?true:false
+}
+
+// use our function 
+ if(canDrive(age)){
+    alert("yes you can drive")
+ }else{
+    alert("you cannot drive")
+ }
+
+ 
 

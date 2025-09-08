72f62b4f1f42e89da67ea5c9ad96063bc485c6a7
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-09 00:18:12 +0530

    add distription

--- PracticeCode/31_wanlkingDOM.js
+++ PracticeCode/31_wanlkingDOM.js
@@ -1,5 +1,14 @@
 
-// jab bhi ham ek HTML page ko access karte he and bahut sare elements ko js se manupulate karne ki kosis karte he 
-// to waha hame dom manupulaation karna padta he 
+/* 
+jab bhi ham ek HTML page ko access karte he and bahut sare elements ko js se manupulate karne ki kosis karte he 
+to waha hame dom manupulaation karna padta he 
 
-// Dom manupulaation - for example ke liye ham koi website kholeneg and uske andar koi changes karne ki kosis karte he like color chnage etc. 
+Dom manupulaation - for example ke liye ham koi website kholeneg and uske andar koi changes karne ki kosis karte he like color chnage etc. 
+
+dom tree object  - apki jo html hai usko convert kar diya gaya he ek js object  me and usko ek object ki tarah use kiya ja sakta he 3 type ke main nodes hote he 
+1 text node 
+2 element node 
+3 comment node
+ a text node is always a leaf of the tree 
+ 
+*/

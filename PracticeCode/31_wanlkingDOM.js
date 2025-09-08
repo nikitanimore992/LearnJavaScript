29a37feef577a9387479b87fac7bad950ff8aead
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-08 23:59:52 +0530

    diecribe dom

---
+++ PracticeCode/31_wanlkingDOM.js
@@ -0,0 +1,5 @@
+
+// jab bhi ham ek HTML page ko access karte he and bahut sare elements ko js se manupulate karne ki kosis karte he 
+// to waha hame dom manupulaation karna padta he 
+
+// Dom manupulaation - for example ke liye ham koi website kholeneg and uske andar koi changes karne ki kosis karte he like color chnage etc. 

42c59f6853b961f696b8315cfb3eaa244959ea48
nikitanimore992 <992nikitanimore@gmail.com>
2026-01-05 00:17:03 +0530

    element only navigation

---
+++ PracticeCode/34_element_only_Navigation.js
@@ -0,0 +1,3 @@
+let b = document.body;
+console.log("First child of b is ",b.firstChild); // Text node (whitespace)
+console.log("First element child of b is ",b.firstElementChild); // First element node
